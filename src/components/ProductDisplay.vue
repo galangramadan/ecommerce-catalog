917f847df278f7cdbda15e26c749dd65a0a12231
galangramadan <galangynr@gmail.com>
2023-08-13 17:43:49 +0700

    fix: styling

--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="container">
-    <div class="tile-background"></div>
+    <div
+      v-if="men || women"
+      class="tile-background"
+      :class="{ menSection: men, womenSection: women }"
+    ></div>
+    <div v-else class="default-background"></div>
     <div class="section">
       <div v-if="loading" class="wrapper">
         <div class="loader"></div>
       </div>
       <div
         v-else-if="Object.keys(data).length == 0 && loading == false"
-        style="
-          width: 100%;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        "
+        class="wrapperNoProduct"
       >
         <div style="display: flex; gap: 200px; margin-top: 100px">
           <img src="../assets/images/aliskiri.png" />
@@ -28,19 +27,7 @@
         <div style="position: absolute; top: auto">
           <div style="display: flex; flex-direction: column">
             <p>This Product is unavailable to show</p>
-            <button
-              style="
-                width: 100%;
-                margin-top: 15px;
-                height: 42px;
-                font-size: 20px;
-                font-weight: 600;
-                border-radius: 4px;
-                border: 3px solid #000;
-                background: none;
-              "
-              @click="handleClick(1)"
-            >
+            <button class="defaultButton" @click="handleClick(1)">
               Next product
             </button>
           </div>
@@ -50,7 +37,7 @@
         <div class="image">
           <img :src="data.image" width="300" />
         </div>
-        <div class="content">
+        <div class="content" :class="{ menColor: men, womenColor: women }">
           <div class="content-body">
             <h3>{{ data.title }}</h3>
             <div
@@ -64,6 +51,8 @@
               <div class="rating">
                 <span>{{ data.rating.rate }}/5</span>
                 <svg
+                  v-for="i in roundedRate"
+                  :key="i"
                   xmlns="http://www.w3.org/2000/svg"
                   width="18"
                   height="18"
@@ -73,15 +62,8 @@
                   <circle cx="9" cy="9" r="9" fill="currentColor" />
                 </svg>
                 <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="18"
-                  viewBox="0 0 18 18"
-                  fill="none"
-                >
-                  <circle cx="9" cy="9" r="9" fill="currentColor" />
-                </svg>
-                <svg
+                  v-for="i in emptyRate"
+                  :key="i + 5"
                   xmlns="http://www.w3.org/2000/svg"
                   width="18"
                   height="18"
@@ -98,13 +80,23 @@
                 </svg>
               </div>
             </div>
-            <p>{{ data.description }}</p>
+            <p v-if="data.description.length > 500">
+              {{ data.description.slice(0, 500) }}...
+            </p>
+            <p v-else>{{ data.description }}</p>
           </div>
           <div class="content-footer">
             <h3>${{ data.price }}</h3>
             <div class="navigation">
-              <button>Buy now</button>
-              <button @click="handleClick(1)">Next product</button>
+              <button :class="{ menButton: men, womenButton: women }">
+                Buy now
+              </button>
+              <button
+                :class="{ menButton: men, womenButton: women }"
+                @click="handleClick(1)"
+              >
+                Next product
+              </button>
             </div>
           </div>
         </div>
@@ -125,6 +117,8 @@ export default {
       data: {},
       men: false,
       women: false,
+      roundedRate: null,
+      emptyRate: null,
       error: true,
       loading: false,
     };
@@ -149,13 +143,19 @@ export default {
           switch (result.category) {
             case "men's clothing":
               this.men = true;
+              this.women = false;
               break;
             case "women's clothing":
               this.women = true;
+              this.men = false;
           }
           this.data = result;
+          this.roundedRate = Math.round(result.rating.rate);
+          this.emptyRate = 5 - this.roundedRate;
         } else {
           this.data = {};
+          this.men = false;
+          this.women = false;
         }
         this.loading = false;
       } catch (error) {
